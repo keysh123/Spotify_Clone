2e96016906cd5fe948a5eb41ecc18cc8c3b86338
keysh123 <keyashah126@gmail.com>
2025-07-16 00:31:22 +0530

    navbar player done ui

---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { albumsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
+
+const DisplayHome = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="mb-4 ">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((album, idx) => {
+            return (
+              <AlbumItem
+                key={idx}
+                img={album.image}
+                desc={album.desc}
+                id={album.id}
+                name={album.name}
+              />
+            );
+          })}
+        </div>
+        <div className="flex overflow-auto">
+          {albumsData.map((album, idx) => {
+            return (
+              <AlbumItem
+                key={idx}
+                img={album.image}
+                desc={album.desc}
+                id={album.id}
+                name={album.name}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DisplayHome;
